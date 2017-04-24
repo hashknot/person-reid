07b878c90f1b0458504524f3febd5b8e91cbe96a
Jitesh Kamble <io@jite.sh>
2017-04-24 16:38:47 -0700

    Use paper cross_diff

--- viper/cross_diff.py
+++ viper/cross_diff.py
@@ -21,55 +21,34 @@ def shape(t):
     s = t.get_shape()
     return tuple([s[i].value for i in range(0, len(s))])
 
+def get_conv_img(t, ksize):
+    shape = t.get_shape().as_list()
+    N = shape[0]
+    H = shape[1]
+    W = shape[2]
+    C = shape[3]
+    patches = tf.extract_image_patches(t,
+                                       [1, ksize[0], ksize[1], 1],
+                                       [1, 1, 1, 1],
+                                       [1, 1, 1, 1],
+                                       padding='SAME')
+    patches = tf.reshape(patches, [N, H, W, ksize[0], ksize[1], C])
+    patches = tf.transpose(patches, [0, 1, 3, 2, 4, 5])
+    patches = tf.reshape(patches, [N, H*ksize[0], W*ksize[1], C])
+    return patches
+
 def cross_difference(a, b):
-    # shape = a.get_shape()
-    shape = (2, 3, 2, 2)
+    shape = a.get_shape().as_list()
     N = shape[0]
     H = shape[1]
     W = shape[2]
     C = shape[3]
     kernel_size = 5
 
-    b_padded = tf.pad(b, [[0, 0], [2, 2], [2, 2], [0, 0]], "CONSTANT")
-
-    i0 = tf.constant(0)
-    m0 = tf.TensorArray(dtype=a.dtype, size=N*H*W*C*kernel_size*kernel_size)
-    cond = lambda i, o, a, b_padded: tf.less(i, N*H*W*C)
-
-    def body(i, out, a, b_padded):
-        c = tf.mod(i, C)
-        remainder = tf.floordiv(i, C)
-
-        w = tf.mod(remainder, W)
-        remainder = tf.floordiv(remainder, W)
-
-        h = tf.mod(remainder, H)
-        remainder = tf.floordiv(remainder, H)
-
-        n = tf.mod(remainder, N)
-
-        a_scalar = a[n, h, w, c]
-        b_stride = tf.strided_slice(b_padded,
-                                    [n, h, w, c],
-                                    [n+1, h+kernel_size, w+kernel_size, c+1],
-                                    [1, 1, 1, 1])
-        b_stride = tf.reshape(b_stride, [kernel_size, kernel_size])
-        # cross_diff = tf.map_fn(lambda x: a_scalar - x, b_stride)
-        # cross_diff = tf.reshape(cross_diff, [kernel_size, kernel_size])
-        out = out.write(i, b_stride)
-        i = tf.add(i, 1)
-        return i, out, a, b_padded
-
-    ret = tf.while_loop(cond,
-                        body,
-                        [i0, m0, a, b_padded],
-                        )
-
-    out = ret[1].stack()
-    cross_diff = tf.reshape(out, (N, H, W, C, kernel_size, kernel_size))
-    cross_diff = tf.transpose(cross_diff, [0, 1, 4, 2, 5, 3])
-    cross_diff = tf.reshape(cross_diff, (N, H*kernel_size, W*kernel_size, C))
-    return cross_diff
+    a_resize = tf.image.resize_images(a, [H*kernel_size, W*kernel_size],
+                                      method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    b_resize = get_conv_img(b, [kernel_size, kernel_size])
+    return tf.subtract(a_resize, b_resize)
 
 def cross_difference2(a, b):
     shape = a.get_shape().as_list()
@@ -80,30 +59,10 @@ def cross_difference2(a, b):
     C = shape[3]
     kernel_size = 5
 
-    a_resize = tf.image.resize_images(a, [H*kernel_size, W*kernel_size], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    a_resize = tf.image.resize_images(a, [H*kernel_size, W*kernel_size],
+                                      method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     b_resize = tf.image.resize_images(b, [H*kernel_size, W*kernel_size])
     return tf.subtract(a_resize, b_resize)
-
-    # a_t = tf.transpose(a, [0, 3, 1, 2])
-    # a_upsample = tf.reshape(a_t, [-1, 1])
-    #
-    # a_upsample = tf.tile(a_upsample, [1, kernel_size])
-    # a_upsample = tf.reshape(a_upsample, [N*C*H, W*kernel_size])
-    #
-    # a_upsample = tf.tile(a_upsample, [1, kernel_size])
-    # a_upsample = tf.reshape(a_upsample, [N*C*H*kernel_size, W*kernel_size])
-    # a_upsample = tf.reshape(a_upsample, [N, C, H*kernel_size, W*kernel_size])
-    # a_upsample = tf.transpose(a_upsample, [0, 2, 3, 1])
-
-    # return a_upsample
-    #
-    # idx = tf.reshape(foo, [-1, 1])
-    # idx = tf.tile(idx, [1, 25])
-    # idx = tf.reshape(idx, [h, w, 5, 5])
-    #
-    # cross_diff = tf.reshape(out, (N, H, W, C, kernel_size, kernel_size))
-    # cross_diff = tf.reshape(cross_diff, (N, H*kernel_size, W*kernel_size, C))
-    # return cross_diff
 
 if __name__ == '__main__':
     w  = tf.Variable([[[[1.,2.], [2.,3.]],
@@ -123,7 +82,7 @@ if __name__ == '__main__':
     # w = tf.ones([128, 37, 12, 25])
     # w2 = tf.zeros([128, 37, 12, 25])
 
-    r = cross_difference2(w2, w)
+    r = cross_difference(w2, w)
 
 
     init_op = tf.global_variables_initializer()
